b5cec86ab33b21317acf893a44c085d191baa835
Tatiana Quintana <tatsqui@gmail.com>
2019-06-18 15:25:45 -0700

    npm installed, adding text fields to playersubmissionform

--- src/components/Game.js
+++ src/components/Game.js
@@ -8,6 +8,10 @@ class Game extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      finalSubmission: [],
+      recentLine: ''
+    }
   }
 
   render() {
@@ -34,7 +38,7 @@ class Game extends Component {
 
         <RecentSubmission />
 
-        <PlayerSubmissionForm />
+        <PlayerSubmissionForm fields={FIELDS} />
 
         <FinalPoem />
 
--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -5,29 +5,44 @@ class PlayerSubmissionForm extends Component {
 
   constructor(props) {
     super(props);
+    const getFields = {}
+    props.fields.forEach(field => {
+      if (field.key) {
+        getFields[field.key] = '';
+      }
+    })
+    this.state = getFields
+  }
+
+  handleSubmitButton = (event) => {
+    event.preventDefault();
+
+  }
+
+  onValueChange = (event) => {
+    const { name, value } = event.target;
+
+    this.setState({[name]: value});
   }
 
   render() {
-
+    const inputFields = this.props.fields.map( (field, i) => {
+      if (field.key) {
+        return <input key={i} name={field.key} value={this.state[field.key]} onChange={this.onValueChange} placeholder="adjective" type="text" />
+      } else {
+        return field
+      }
+    });
     return (
       <div className="PlayerSubmissionForm">
-        <h3>Player Submission Form for Player #{  }</h3>
+        <h3>Player Submission Form for Player #{}</h3>
 
         <form className="PlayerSubmissionForm__form" >
-
           <div className="PlayerSubmissionForm__poem-inputs">
-
-            {
-              // Put your form inputs here... We've put in one below as an example
-            }
-            <input
-              placeholder="hm..."
-              type="text" />
-
+          {inputFields}
           </div>
-
           <div className="PlayerSubmissionForm__submit">
-            <input type="submit" value="Submit Line" className="PlayerSubmissionForm__submit-btn" />
+            <input type="submit" onSubmit={this.handleSubmitButton} value="Submit Line" className="PlayerSubmissionForm__submit-btn" />
           </div>
         </form>
       </div>
